3c8b57b4f556b2eae10568991a963fa70d44b810
nalghamdi <nalghamdi_@hotmail.com>
2019-01-14 17:20:59 +0300

    startup

---
+++ API-backend/db/seed.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS school_db_db;
+CREATE DATABASE school_db_db;
+\c school_db_db
+
+
+CREATE TABLE schools (
+   id serial primary key,
+   title varchar, 
+   address varchar
+
+);
+
+CREATE TABLE bakery (
+   id serial primary key,
+   title varchar, 
+   address varchar,
+   img varchar
+);
+
+CREATE TABLE subscription (
+   id serial primary key,
+   name varchar, 
+-- foreign
+);
+
+
+
+CREATE TABLE meals (
+   id serial primary key,
+   name varchar, 
+   description varchar
+   img varchar 
+);
+
+CREATE TABLE ingredients (
+   id serial primary key,
+   name varchar, 
+   calories int
+);
+
+CREATE TABLE meals_ingredients (
+  meal_id int,
+  ingredient_id int,
+  foreign key (meal_id) references meals,
+  foreign key (ingredient_id) references ingredients
+);
+
+CREATE TABLE workout (
+  id serial primary key,
+  name varchar, 
+  description varchar,
+  calories_burned int,
+  minutes int,
+);
+
+
+
+
+
+
+
+-- INSERT INTO  () VALUES ();
